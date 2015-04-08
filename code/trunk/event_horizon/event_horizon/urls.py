ddbc3f01af24c657ed3532d93615260e1b2eb4c8
Lucas Magnum <lucasmagnum2@gmail.com>
2015-04-07 22:31:30 -0300

    Pep8 nas urls e removido código morto.

--- code/trunk/event_horizon/event_horizon/urls.py
+++ code/trunk/event_horizon/event_horizon/urls.py
@@ -5,20 +5,12 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'event_horizon.views.home', name='home'),
-    # url(r'^event_horizon/', include('event_horizon.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Root redirect to admin
+urlpatterns = patterns(
+    '',
     url(r'^$', RedirectView.as_view(url='/dashboard/'), name='root-admin'),
 
     # Apps urls
